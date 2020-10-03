964ba83e03c2dbe3707f239f7db6d513d2d3f488
HoangTu <le.hoang.tu@sun-asterisk.com>
2020-10-03 15:37:00 +0700

    abc

--- app/Scraper/TGDD.php
+++ app/Scraper/TGDD.php
@@ -7,39 +7,44 @@ use App\ArticleContent;
 use App\Product;
 use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
-
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Response;
 class TGDD
 {
 
     public function scrape()
     {
-        $url = 'https://thethao247.vn/bong-da-anh-c8/';
+        // $url = 'https://thethao247.vn/bong-da-anh-c8/';
 
-        $client = new Client();
+        // $client = new Client();
 
-        $crawler = $client->request('GET', $url);
+        // $crawler = $client->request('GET', $url);
 
-        $crawler->filter('ul.playlist_hl_video li')->each(
-            function (Crawler $node) {
-                $title = $node->filter('h3 a')->text();
-                $hot_content = $node->filter('p')->text();
-                $sub_url = $node->filter('h3 a')->attr('href');
-                $article = new Article;
-                $article->title = $title;
-                $article->hot_content = $hot_content;
-                $article->save();
+        // $crawler->filter('ul.playlist_hl_video li')->each(
+        //     function (Crawler $node) {
+        //         $title = $node->filter('h3 a')->text();
+        //         $hot_content = $node->filter('p')->text();
+        //         $sub_url = $node->filter('h3 a')->attr('href');
+        //         $article = new Article;
+        //         $article->title = $title;
+        //         $article->hot_content = $hot_content;
+        //         $article->save();
 
-                $sub_client = new Client();
-                $sub_req = $sub_client->request('GET', $sub_url);
-                $sub_req->filter('#main-detail p')->each(
-                    function(Crawler $node){
-                        $article = Article::all()->last();
-                        $sub_content = $node->text();
-                        $article->contents()->create(['sub_content' => $sub_content]);
-                    }
-                );
+        //         $sub_client = new Client();
+        //         $sub_req = $sub_client->request('GET', $sub_url);
+        //         $sub_req->filter('#main-detail p')->each(
+        //             function(Crawler $node){
+        //                 $article = Article::all()->last();
+        //                 $sub_content = $node->text();
+        //                 $article->contents()->create(['sub_content' => $sub_content]);
+        //             }
+        //         );
 
 
-            });
+        //     });
+
+        $url = '127.0.0.1:5000';
+        $request = Http::post($url.'/check', ['a' => 'b']);
+        echo $request->body();
     }
 }
